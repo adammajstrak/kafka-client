89f014d148383bd0fdf8b450d2288d169a34d57f
Majstrak, A. (Adam) <adam.majstrak@ing.com>
2023-12-22 14:16:29 +0100

    init

---
+++ KafkaClient/KafkaClient/Program.cs
@@ -0,0 +1,30 @@
+﻿using Confluent.Kafka;
+using System.Text.Json;
+
+var config = new ProducerConfig { BootstrapServers = "localhost:29092" };
+
+using (var p = new ProducerBuilder<Null, string>(config).Build())
+{
+    try
+    {
+        var objToSend = new
+        {
+            name = "adam"
+        };
+
+        var message = JsonSerializer.Serialize(objToSend);
+
+        var dr = await p.ProduceAsync("Test", new Message<Null, string> 
+        {
+            Value = message 
+        });
+
+        Console.WriteLine($"Delivered '{dr.Value}' to '{dr.TopicPartitionOffset}'");
+    }
+    catch (ProduceException<Null, string> e)
+    {
+        Console.WriteLine($"Delivery failed: {e.Error.Reason}");
+    }
+}
+
+Console.ReadKey();
